6250345385d3a2df790c79907c16b42b7ba563d1
moom <1374073829@qq.com>
2018-10-11 21:52:21 +0800

    这是第二版

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require("gulp");
+// var sass = require("gulp-sass");
+//repuire:相当于<script src="gulp.js"></script>
+//定义一个复制文件的任务
+//task函数的第一个参数：copyHtml是任务名
+//task函数的第二个参数：function是任务copyHtml对应的功能
+gulp.task("copyHtml",function(){
+	gulp.src("index.html")
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\item"));
+});
+
+gulp.task("copyImg",function(){
+	gulp.src("img/**/*")
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\item\\img"));
+});
+gulp.task("copyCss",function(){
+	gulp.src("css/**/*")
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\item\\css"));
+});
+gulp.task("copyJs",function(){
+	gulp.src("js/**/*")
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\item\\js"));
+});
+gulp.task("copyFont",function(){
+	gulp.src("font/**/*")
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\item\\font"));
+});
+// 监听
+gulp.task("watchall",function(){
+	gulp.watch("index.html",["copyHtml"]);
+	gulp.watch("img/**/*",["copyImg"]);
+	gulp.watch("css/**/*",["copyCss"]);
+	gulp.watch("js/**/*",["copyJs"]);
+	gulp.watch("font/**/*",["copyFont"]);
+});
+
--- js/index.js
+++ js/index.js
@@ -25,10 +25,7 @@ $.each(
 			}
 		);
 });
-// $("#result").mouseout(function(){
-// 	$("#result").css({display:"none"});
-// });
-// 点击关闭
+// 搜索后预结果中的点击关闭
 $(".result_bottom a").click(function(){
 	$("#result").css({display:"none"});
 });
@@ -77,26 +74,178 @@ $.each(
 					background:" #000",
 					color:"#fff"
 				});
-				$("span").css({
-					width:"0px",
-					height:"0px",
-					border:"6px solid transparent",
-					position: "absolute",
-					top:"35px",
-					left:"70px"
-				});
+				$(".span_tra").css({display:"block"});
 			},
 			function(){
 				$(this).css({
 					background:" #fff",
 					color:"#000"
 				});
-				$("span").css({
-					width:"0px",
-					height:"0px"
-					
-				});	
+				$(".span_tra").css({display:"none"});
 			}
 		);
 });
 
+// 划过小人显示的登录注册
+$("#ren").hover(
+	function(){
+		$(".reg").css({
+			display:"block"
+		});
+		$(this).css({background:"#e50065"});
+		$(".reg").mouseover(function(){
+			$(".reg").css({
+				display:"block"
+			});
+			$("#ren").css({background:"#e50065"});
+		});
+		$(".reg").mouseout(function(){
+			$(".reg").css({
+				display:"none"
+			});
+			$("#ren").css({background:"#000"});
+		});
+	},
+	function(){
+		$(".reg").css({
+			display:"none"
+		});
+		$(this).css({background:"#000"});
+	}
+);
+$(".denglu").hover(
+	function(){
+		$(this).css({background:"#fff",color:"#c10055"});
+
+	},
+	function(){
+		$(this).css({background:"#c10055",color:"#fff"});
+	}
+);
+$(".zuce").hover(
+	function(){
+		$(this).css({background:"#c10055",color:"#fff"});
+		$(".denglu").css({background:"#fff",color:"#c10055"});
+
+	},
+	function(){
+		$(this).css({background:"#fff",color:"#c10055"});
+		$(".denglu").css({background:"#c10055",color:"#fff"});
+
+	}
+);
+// 划过购物车
+$(".sum").hover(
+	function(){
+		$(".shopping").css({display:"block"});
+		$(".sum").css({background:"#e50065"});
+		$(".shopping").mouseover(function(){
+			$(".shopping").css({display:"block"});
+			$(".sum").css({background:"#e50065"});
+		});
+		$(".shopping").mouseout(function(){
+			$(".shopping").css({display:"none"});
+			$(".sum").css({background:"#000"});
+		});
+	},
+	function(){
+		$(".shopping").css({display:"none"});
+		$(".sum").css({background:"#000"});
+	}
+);
+// 划过心
+$("#xin").hover(
+	function(){
+		$(this).css({background:"#e50065"});
+	},
+	function(){
+		$(this).css({background:"#000"});
+	}
+);
+// 划过微信
+$("#weixin").hover(
+	function(){
+		$(".wenxinCode").css({display:"block"});
+		$(this).css({background:"#e50065"});
+		$(".wenxinCode").mouseover(function(){
+			$(".wenxinCode").css({display:"block"});
+			$("#weixin").css({background:"#e50065"});
+
+		});
+		$(".wenxinCode").mouseout(function(){
+			$(".wenxinCode").css({display:"none"});
+			$("#weixin").css({background:"#000"});
+
+		});
+	},
+	function(){
+		$(".wenxinCode").css({display:"none"});
+		$(this).css({background:"#000"});
+	}
+);
+// 划过耳机
+$("#erji").hover(
+	function(){
+		$(this).css({background:"#e50065"});
+	},
+	function(){
+		$(this).css({background:"#262626"});
+
+	}
+);
+// 划过返回顶部
+$("#backTop").hover(
+	function(){
+		$(".back").css({display:"block"});
+		$(this).css({background:"#e50065"});
+	},
+	function(){
+		$(".back").css({display:"none"});
+		$(this).css({background:"#000"});
+	}
+);
+// 点击返回顶部
+$("#backTop").click(
+	function(){
+		$('body,html').animate({scrollTop:0},0);
+        return false;
+	}
+);
+//划过顶部的微信
+$("#weixin2").hover(
+	function(){
+		$(".wei2").css({display:"block"});
+	},
+	function(){
+		$(".wei2").css({display:"none"});
+	}
+);
+//划过顶部的微博
+$("#weibo2").hover(
+	function(){
+		$(".weibo").css({display:"block"});
+	},
+	function(){
+		$(".weibo").css({display:"none"});
+	}
+);
+//划过顶部的手机版
+$("#phone2").hover(
+	function(){
+		$(".shouji").css({display:"block"});
+		$(".phone").attr({src:"img/redphone.jpg"});
+	},
+	function(){
+		$(".shouji").css({display:"none"});
+		$(".phone").attr({src:"img/phone.jpg"});
+	}
+);
+//划过顶部的购物车
+$("#car2").hover(
+	function(){
+		$(".shopCar").css({display:"block"});
+	},
+	function(){
+		$(".shopCar").css({display:"none"});
+	}
+);
